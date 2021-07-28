8bbd81e73b3cb1e01b271d92daf93cc605d1d568
jnnastti <janna.sangaletti6@gmail.com>
2021-07-28 20:40:38 -0300

    ⚙ controller

---
+++ server/controller/EmpresaController.php
@@ -0,0 +1,37 @@
+<?php
+
+    session_start();
+
+    require('../../../server/config.php');
+    
+    include('../../../server/src/Empresa.php');
+    include('../../../server/redirect.php');
+
+    $empresa = new Empresa($db);
+    $itemEmpresa = $empresa->listarEmpresas($_SESSION['usuarioLogin']);
+
+    $action = (isset($_REQUEST['action'] )) ? $_REQUEST['action']  : '';
+    $nomeEmpresa = (isset($_GET['id'])) ? $empresa->selecionarEmpresa($_GET['id'], 'editar') : '';
+
+    switch($action) {
+        case 'deletar': {
+            $empresa->deletarEmpresa($_POST['id'], $_SESSION['usuarioLogin']);
+            redireciona('./index.php');
+            break;
+        }
+        case 'editar': {
+            $empresa->editarEmpresa($_POST['nome'], $_POST['id']);
+            redireciona('./index.php');
+            break;
+        }
+        case 'cadastrar': {
+            $empresa->cadastrarEmpresa($_POST['nome'], $_SESSION['usuarioLogin']);
+            redireciona('./index.php');
+            break;
+        }
+        case 'selecionar': {
+            $empresa->selecionarEmpresa($_POST['id'], 'selecionar');
+            redireciona('../projetos/index.html');
+        }
+    }
+?>
--- web/pages/empresas/index.php
+++ web/pages/empresas/index.php
@@ -1,43 +1,11 @@
 <?php
-    session_start();
 
-    require('../../../server/config.php');
-    
-    include('../../../server/src/Empresa.php');
-    include('../../../server/redirect.php');
+    session_start();
+    require_once('../../../server/controller/EmpresaController.php');
 
     if(empty($_SESSION['usuarioLogin'])) {
         redireciona('../login/login.php');
     }
-
-    $empresa = new Empresa($db);
-    $itemEmpresa = $empresa->listarEmpresas($_SESSION['usuarioLogin']);
-
-    $action = (isset($_REQUEST['action'] )) ? $_REQUEST['action']  : '';
-    $nomeEmpresa = (isset($_GET['id'])) ? $empresa->selecionarEmpresa($_GET['id'], 'editar') : '';
-
-    switch($action) {
-        case 'deletar': {
-            $empresa->deletarEmpresa($_POST['id'], $_SESSION['usuarioLogin']);
-            redireciona('./index.php');
-            break;
-        }
-        case 'editar': {
-            $empresa->editarEmpresa($_POST['nome'], $_POST['id']);
-            redireciona('./index.php');
-            break;
-        }
-        case 'cadastrar': {
-            $empresa->cadastrarEmpresa($_POST['nome'], $_SESSION['usuarioLogin']);
-            redireciona('./index.php');
-            break;
-        }
-        case 'selecionar': {
-            $empresa->selecionarEmpresa($_POST['id'], 'selecionar');
-            redireciona('../projetos/index.html');
-        }
-    }
-
 ?>
 
 <html>
